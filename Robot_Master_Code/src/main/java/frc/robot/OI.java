17a51c7fb4b823459588981f4c218b7f6bfaa753
Westside FRC Laptop <59748021+WestsideFRC@users.noreply.github.com>
2020-02-21 17:15:15 -0600

    change mapping

--- Robot_Master_Code/src/main/java/frc/robot/OI.java
+++ Robot_Master_Code/src/main/java/frc/robot/OI.java
@@ -25,7 +25,7 @@ public class OI {
   public static final int JOYST_LEFT_X = 1;
   public static final int JOYST_RIGHT_Y = 2;
   public static final int JOYST_RIGHT_X = 3;
-  public static final int SHOULDER_AXIS = 3; //TODO: change numbers to match what should be their!!!
+  public static final int SHOULDER_AXIS = 5; //TODO: change numbers to match what should be their!!!
   public static final int DPAD_X = 1;
   public static final int DPAD_Y = 1;
 
