cb27a12b4e0bc7cda1aed158dc535af0768ef1ca
Westside FRC Laptop <59748021+WestsideFRC@users.noreply.github.com>
2020-02-22 09:33:09 -0600

    added intake sensitivity

--- Robot_Master_Code/src/main/java/frc/robot/commands/TeleOpIntake.java
+++ Robot_Master_Code/src/main/java/frc/robot/commands/TeleOpIntake.java
@@ -30,26 +30,6 @@ public class TeleOpIntake extends Command {
   // Called repeatedly when this Command is scheduled to run
   @Override
   protected void execute() {
-    // speed is way too high try not going percentage 0-100 and try decimals ex: .5 = 50%
-    // if(oi.controller.getRawButtonPressed(RobotMap.INTAKE_IN)){
-    //   Robot.intake.setIntakeSpeed(0.5);
-    // }else if(oi.controller.getRawButtonReleased(RobotMap.INTAKE_IN)){
-    //   Robot.intake.setIntakeSpeed(0);
-    // }
-    // if(oi.controller.getRawButtonPressed(RobotMap.INTAKE_OUT)){
-    //    Robot.intake.setIntakeSpeed(-0.5);
-    // }else if(oi.controller.getRawButtonReleased(RobotMap.INTAKE_OUT)){
-    //   Robot.intake.setIntakeSpeed(0);
-    // }
-
-
-    // if(oi.controller.getRawAxis(OI.LT_RT_AXIS) > -.5) {
-    //   Robot.intake.setPosition(false);
-    // }
-    // if(oi.controller.getRawAxis(OI.LT_RT_AXIS) < -.5) {
-    //   Robot.intake.setPosition(true);
-    // }
-
     if(oi.controller.getRawButtonPressed(RobotMap.INTAKE_UP)){
       Robot.intake.setPositionDown();
     }
@@ -57,33 +37,8 @@ public class TeleOpIntake extends Command {
       Robot.intake.setPositionUp();
     }
 
-    Robot.intake.setIntakeSpeed(oi.controller.getRawAxis(RobotMap.INTAKE_SPEED));
-
-    //double x = oi.controller.getRawAxis(RobotMap.INTAKE_IN_AND_OUT);
-    //
-    // if(x<-.1){
-    //
-    // } 
-    // else if(x>.1){
-    //   if(x>.9){
-    //     Robot.intake.setIntakeSpeed(.5);
-    //   }
-    //   else if(x>.7){
-    //     Robot.intake.setIntakeSpeed(.3);
-    //   }
-    //   else if(x>.4){
-    //     Robot.intake.setIntakeSpeed(.1);
-    //   } 
-    //   else {
-    //
-    //   }
-    // } 
-    // else {
-    //   Robot.intake.setIntakeSpeed(0);
-    // }
-
-
-
+    Robot.intake.setIntakeSpeed(RobotMap.INTAKE_SENSITIVITY*oi.controller.getRawAxis(RobotMap.INTAKE_SPEED));
+    
   }
 
   // Make this return true when this Command no longer needs to run execute()
